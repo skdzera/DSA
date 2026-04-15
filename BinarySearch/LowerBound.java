c43113926eb76808644f9846eebc73f69d964d28
saksham <88574021+skdzera@users.noreply.github.com>
2026-04-15 15:27:15 +0530

    Add files via upload

---
+++ BinarySearch/LowerBound.java
@@ -0,0 +1,23 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+         
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(arr[mid]>=target){
+                end = mid-1;
+            }
+            else if(arr[mid]<target){ 
+                start = mid+1;
+            }
+            
+        }
+        return start;
+    
+    }
+}
+
